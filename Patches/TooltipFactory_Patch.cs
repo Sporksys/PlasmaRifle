9265215c3fb3417347c1bb563e1b91037f7c4023
Sporksys <73980755+Sporksys@users.noreply.github.com>
2020-12-12 14:10:14 -0800

    Update and rename TooltipFactory_ItemCommons_Patch.cs to TooltipFactory_Patch.cs

--- Patches/TooltipFactory_Patch.cs
+++ Patches/TooltipFactory_Patch.cs
@@ -5,11 +5,12 @@ using System.Text;
 namespace PlasmaRifle
 {
     [HarmonyPatch(typeof(TooltipFactory), "ItemCommons")]
-    class TooltipFactory_ItemCommons_Patch
+    class ItemCommons_Patch
     {
+        [HarmonyPrefix]
         private static bool Prefix(StringBuilder sb, TechType techType, GameObject obj)
         {
-            if(techType == Main.techType)
+            if(techType == Main.Main.Mk1TechType || techType == Main.Mk2TechType || techType == Main.Mk3TechType || techType == Main.FullCartridgeTechType)
             {
                 string title = Language.main.Get(techType);
                 sb.AppendFormat("<size=25><color=#ffffffff>{0}</color></size>", title);
@@ -28,10 +29,17 @@ namespace PlasmaRifle
                         sb.AppendFormat("\n<size=20><color=#DDDEDEFF>{0}</color></size>", Language.main.Get("BatteryNotInserted"));
                     }
                 }
+                
                 PlasmaRifle plasmaRifle = obj.GetComponent<PlasmaRifle>();
                 if(plasmaRifle != null)
                 {
-                    sb.AppendFormat("\n<size=20><color={0}>{1}</color></size>", ConditionHandler.GetConditionColor(plasmaRifle.condition), plasmaRifle.GetTooltipString());
+                    plasmaRifle.AppendTooltip(sb);
+                }
+                
+                GasCartridge gasCartridge = obj.GetComponent<GasCartridge>();
+                if(gasCartridge != null)
+                {
+                    gasCartridge.AppendTooltip(sb);
                 }
 
                 sb.AppendFormat("\n<size=20><color=#DDDEDEFF>{0}</color></size>", Language.main.Get(TooltipFactory.techTypeTooltipStrings.Get(techType)));
